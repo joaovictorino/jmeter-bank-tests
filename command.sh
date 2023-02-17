49f41ee6e0b048f26ab3ef4e0eab905ebccb6e0f
joaovictorino <jhvictorino@gmail.com>
2023-02-16 21:59:23 -0300

    correção

--- command.sh
+++ command.sh
@@ -1,4 +1,5 @@
 #Interact with API
+docker compose up springapi
 curl -X POST http://localhost:8080/account/123456/1000
 curl http://localhost:8080/account/123456
 curl -X POST http://localhost:8080/transfer/123456/654321/5  
